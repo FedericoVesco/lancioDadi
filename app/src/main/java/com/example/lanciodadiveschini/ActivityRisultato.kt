c30d456a2c2448181044c503c8e54551dc7f6548
FedericoVesco <federicoveschini@gmail.com>
2025-04-29 14:42:31 +0200

    creata la logica vincente

--- app/src/main/java/com/example/lanciodadiveschini/ActivityRisultato.kt
+++ app/src/main/java/com/example/lanciodadiveschini/ActivityRisultato.kt
@@ -1,6 +1,7 @@
 package com.example.lanciodadiveschini
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +13,20 @@ class ActivityRisultato : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_risultato)
 
+        val mioRandom = intent.getIntExtra("RANDOM", -1)
+        val textRisultato = findViewById<TextView>(R.id.textRisultato)
+        val nVincente = random()
+
+        if(mioRandom == nVincente){
+            textRisultato.text = "Complimenti hai vinto!"
+        }
+        else{
+            textRisultato.text = "Hai perso, ritenta"
+        }
+
+    }
+
+    private fun random(): Int{
+        return (1..6).random()
     }
 }
